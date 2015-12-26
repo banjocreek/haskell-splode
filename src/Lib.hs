94d441745fd5ff925bfa9d2898ec163318ed3e43
Greg Wiley <aztec.rex@jammm.com>
2015-12-26 10:55:45 -0800

    checkpoint, still wandering

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,8 @@
 module Lib
     ( place
     , Cell (Cell)
+    , cyclic
+    , square
     ) where
 
 
@@ -8,3 +10,17 @@ data Cell = Cell Int [Cell] deriving (Show, Eq)
 
 place :: Cell -> Cell
 place (Cell x cs) = Cell (x+1) cs
+
+
+square :: [Cell]
+square = [ab, ab, ba, bb]
+  where aa = Cell 0 [ab, ba]
+        ab = Cell 0 [aa, bb]
+        ba = Cell 0 [aa, bb]
+        bb = Cell 0 [ab, ba]
+
+
+cyclic :: [Int]
+cyclic = let x = 0 : y
+             y = 1 : x
+         in x
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,7 +5,8 @@ main :: IO ()
 main = do
   putStrLn ""
   quickCheck placeIncrementsCount
+  print square
   putStrLn ""
 
 placeIncrementsCount :: Int -> Bool
-placeIncrementsCount n = place (Cell n [1,2,3]) == Cell (n+1) [1,2,3]
+placeIncrementsCount n = place (Cell n [Cell 0 [], Cell 1 []]) == Cell (n+1) [Cell 0 [], Cell 1 []]
